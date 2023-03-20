a63eac9630eb8a0faa86c77ea6a2362fe340dd92
Rushikesh Patil <patilrushikesh8683@gmail.com>
2023-03-20 16:25:58 +0530

    Final React Assifnment (MCT)

---
+++ src/redux/action.js
@@ -0,0 +1,12 @@
+export const productData = (data)=>{
+    return {
+        type: "productData",
+        data : data
+    }
+}
+export const catagaryData=(data)=>{
+    return{
+        type:"catagaryData",
+        data:data
+    }
+}
